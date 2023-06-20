6617abbde715d9866bd07ce556807752db6cd673
rishav-ghosh <137140969+rishav-ghosh@users.noreply.github.com>
2023-06-20 10:22:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//tip Calculator 
+function calculateTip()
+{
+    var billAmount = document.getElementById("billAmount").value;
+    var serviceQuality = document.getElementById("serviceQuality").value;
+    var numPeople = document.getElementById("totalPeople").value;
+
+    if(billAmount == "" || serviceQuality == 0)
+    {
+        window.alert("Please enter some value to run the machine");
+        return;
+    }
+
+    if(numPeople == "" || numPeople <= 1)
+    {
+        numPeople = 1;
+        document.getElementById("each").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("each").style.display = "block";
+    }
+
+    var total = (billAmount * serviceQuality) / numPeople;
+    total = Math.round(total);
+
+    document.getElementById("totalTip").style.display = "block";
+    document.getElementById("tip").innerHTML = total;
+}
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+document.getElementById("calculate").onclick = function() { calculateTip(); };
